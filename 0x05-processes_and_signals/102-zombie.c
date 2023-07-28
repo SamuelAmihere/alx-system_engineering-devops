67f07ea26f5739bad892947c1fca7f91e09be464
SamuelAmihere <amisam2000@gmail.com>
2023-07-28 11:35:21 -0700

    102-zombie

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,38 @@
-#
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	pid_t zombie_pid;
+	int i;
+
+	for (i = 4; i >= 0; i--)
+	{
+		zombie_pid = fork();
+		if (zombie_pid == NULL)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie_pid);
+	}
+	infinite_while();
+	return (0);
+}
+
+/**
+ * infinite_while - Performs infinite loop
+ * Return: 0 always
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
