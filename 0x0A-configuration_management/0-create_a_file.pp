1e85a3b4bc509cb053829d3f8ce80d1830da4d0c
Samuel Amihere <amisam2000@gmail.com>
2023-11-25 02:20:41 +0800

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creating a file
 
 file { '/tmp/school':
-  content => 'I love Puppet',
   mode    => '0744',
-  owneer  => 'www-data',
+  owner  => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
