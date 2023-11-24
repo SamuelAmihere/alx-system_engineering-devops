a6d45a861de6c85683e1a0da4c7d5ac55c142b9e
Samuel Amihere <amisam2000@gmail.com>
2023-11-25 02:55:15 +0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Installing a package
 
-package { 'Werkzeug':
-  ensure   => '2.2.2',
-  provider => 'pip3',
-}
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
 }
