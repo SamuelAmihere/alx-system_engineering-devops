1b05c61cdd33e41b1d602be67facadd200d7d006
Samuel Amihere <amisam2000@gmail.com>
2023-12-05 14:06:54 +0800

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,15 @@
 # Creating custom HTTP header with Puppet
-exec { 'update':
+exec {'apt-get-update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+package {'nginx':
+  ensure => 'present',
 }
-
-file_line { 'first':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file_line { 'second':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
