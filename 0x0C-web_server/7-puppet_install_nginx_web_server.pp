24b53b69829af5d9369b1ce863d362136dd4a601
Samuel Amihere <amisam2000@gmail.com>
2023-12-07 02:51:36 +0800

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,12 +4,20 @@ package {'nginx':
   ensure => 'present',
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
 }
 
-exec {'301':
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com permanent;/" /etc/nginx/sites-enabled/default':
+exec {'Hello':
+  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {
+  command  => 'sudo sed -i "s/s/server_name _;/server_name _;\n\trewrite ^\/redirect_me \
+wrapped > https:\/\/www.youtube.com permanent;/" /etc/nginx/sites-enabled/default',
   provider => shell,
 }
 
