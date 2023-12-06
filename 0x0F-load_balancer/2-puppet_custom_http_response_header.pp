35bd101423d3d3457219cb5052388b4b93794387
Samuel Amihere <amisam2000@gmail.com>
2023-12-07 05:25:25 +0800

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,28 @@
 # Creating custom HTTP header with Puppet
-exec {'apt-get-update':
-  command => '/usr/bin/apt-get update',
+package { 'nginx':
+  ensure => installed,
 }
-package {'nginx':
+
+file_line { 'a':
   ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
 }
-file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
-exec {'run':
-  command => '/usr/sbin/service nginx restart',
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
