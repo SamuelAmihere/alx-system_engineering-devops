fd4c15acea0447ad6c29fe852668c0d7fdce242d
Samuel Amihere <amisam2000@gmail.com>
2024-01-03 21:53:36 +0800

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,27 +5,24 @@ from sys import argv
 import csv
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
 
-    userId = sys.argv[1]
+    user_id = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos.json()
+                        .format(user_id))
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    todoUser = {}
-    taskList = []
+    userTasks = {}
+    tasks = []
+    for task in todo.json():
+        if task.get('userId') == int(user_id):
+            tasks.append(
+                    {"task": task.get('title'),
+                     "username": user.json().get('username'),
+                     "completed": task.get('completed')})
 
-    for task in todos:
-        if task.get('userId') == int(userId):
-            taskDict = {"task": task.get('title'),
-                        "completed": task.get('completed'),
-                        "username": user.json().get('username')}
-            taskList.append(taskDict)
-    todoUser[userId] = taskList
-
-    filename = userId + '.json'
-    with open(filename, mode='w') as f:
-        json.dump(todoUser, f)
+    userTasks[user_id] = tasks
+    file = f"{}.json".format(user_id)
+    with open(file, mode='w') as fd:
+        json.dump(userTasks, fd)
