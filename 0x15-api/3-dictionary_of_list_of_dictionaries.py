bef44d95d6ec36aef2188f4f9fa6fc06c5cdd35a
Samuel Amihere <amisam2000@gmail.com>
2024-01-03 22:36:31 +0800

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """using this REST API, for a given employee ID"""
 
-import requests
 from sys import argv
+import json
+import requests
 
 if __name__ == "__main__":
-    user_id = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    user_name = user.json().get('name')
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    completed_task = 0
-    tasks = 0
-    for task in todo.json():
-        if task.get('userId') == int(user_id):
-            if task.get('completed'):
-                completed_task += 1
-        tasks += 1
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed_task, tasks))
-    for task in todo.json():
-        if task.get('userId') == int(user_id) and \
-                    task.get('completed'):
-            print('\n'.join(["\t " + task.get('title')]))
+    all_users = requests.get("https://jsonplaceholder.typicode.com/users")\
+            .json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    all_todos = {}
+    for user in all_users:
+        tasks = []
+        for task in todo:
+            if task.get('userId') == user.get('id'):
+                tasks.append({"task": task.get('title'),
+                              "completed": task.get('completed'),
+                              "username": user.get('username')})
+
+        all_todos[user.get('id')] = tasks
+
+    file = 'todo_all_employees.json'
+    with open(file, mode='w') as f:
+        json.dump(all_todos, f)
