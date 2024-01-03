b4905dc0023f429106883ce8de950a2a8519b3dc
Samuel Amihere <amisam2000@gmail.com>
2024-01-03 12:59:55 +0800

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""using this REST API, for a given employee ID"""
+
+
+import requests
+from sys import argv
+
+user_id = argv[0]
+user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId))
+
+user_name = user.json().get('name')
+
+todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+completed_task = 0
+tasks = 0
+
+for task in todo.json():
+    if task.get('userId') == int(user_id):
+        if task.get('completed'):
+            completed_task += 1
+	tasks += 1
+
+print('Employee {} is done with tasks({}/{}):'.format(name, completed_task, tasks))
+
+
+for task in todo.json():
+    if task.get('userId') == int(user_id) and task.get('completed'):
+        print('\n'.join(["\t " + task.get('title')))
+
