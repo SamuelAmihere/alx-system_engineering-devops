3711a80a7fa61fbbf1b49dabb1883a932a1e4301
Samuel Amihere <amisam2000@gmail.com>
2024-01-03 22:19:00 +0800

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """Exports data in the JSON format"""
 
+from sys import argv
+import json
+import requests
+
 if __name__ == "__main__":
 
-    import json
-    import requests
-    import sys
-
-    user_id = sys.argv[1]
+    user_id = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(user_id))
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 
     userTasks = {}
     tasks = []
-    for task in todo.json():
+
+    for task in todo:
         if task.get('userId') == int(user_id):
             tasks.append(
                     {"task": task.get('title'),
                      "username": user.json().get('username'),
                      "completed": task.get('completed')})
-
     userTasks[user_id] = tasks
-    file = f"{}.json".format(user_id)
-    with open(file, mode='w') as fd:
-        json.dump(userTasks, fd)
+
+    file = '{}.json'.format(user_id)
+    with open(file, mode='w') as f:
+        json.dump(userTasks, f)
