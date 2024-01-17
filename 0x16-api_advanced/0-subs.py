fafaffd796f1d47f0d251a062da32a87580cfcaa
Samuel Amihere <amisam2000@gmail.com>
2024-01-17 13:19:25 +0800

    Advanced API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Querying subscribers on a Reddit API ."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Finds the total number of subscribers.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    ua = "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    headers = {
+        "User-Agent": ua
+    }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code == 404:
+        return 0
+    return res.json().get("data").get("subscribers")
