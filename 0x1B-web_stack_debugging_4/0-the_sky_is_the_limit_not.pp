97f5524ac504f112479a243d1b41ee6a4d574c76
Samuel Amihere <amisam2000@gmail.com>
2024-02-23 07:34:45 +0800

    Sky is the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# Increasing the traffic amount an Nginx server.
+
+# Increase the ULIMIT
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  # specifying path for the sed command to exceute
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  # specifying path for the init.d file
+  path    => '/etc/init.d/'
+}
